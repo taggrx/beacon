0be44c1c32c71ca06d569b8c3bd8f50f85fca07d
X <x@taggr.link>
2023-11-26 16:02:56 +0100

    feat: sell order

--- src/backend/order_book/mod.rs
+++ src/backend/order_book/mod.rs
@@ -88,6 +88,60 @@ impl State {
         );
     }
 
+    pub async fn sell(
+        &mut self,
+        seller: Principal,
+        token: TokenId,
+        mut amount: Tokens,
+        limit: Option<E8sPerToken>,
+        time: Timestamp,
+    ) -> Result<(), String> {
+        if amount
+            > *self
+                .pools
+                .get(&token)
+                .ok_or("no token pool")?
+                .get(&seller)
+                .unwrap_or(&0)
+        {
+            return Err("not enough tokens".into());
+        }
+
+        let buy_orders = &mut self
+            .orders
+            .get_mut(&token)
+            .ok_or("no buy orders found")?
+            .buyers;
+
+        let archive = self.order_archive.entry(token).or_default();
+
+        while let Some(mut order) = buy_orders.pop_first() {
+            if limit > Some(order.price) || amount < order.amount {
+                buy_orders.insert(order);
+                break;
+            }
+
+            amount = amount.checked_sub(order.amount).expect("too small amount");
+
+            trade(
+                &mut self.pools,
+                &mut self.icp_pool,
+                seller,
+                order.owner,
+                token,
+                order.amount,
+                order.amount * order.price,
+                self.revenue_account.unwrap(),
+            );
+
+            order.executor = Some(seller);
+            order.executed = time;
+            archive.push(order);
+        }
+
+        Ok(())
+    }
+
     pub async fn buy(
         &mut self,
         buyer: Principal,
@@ -96,60 +150,38 @@ impl State {
         limit: Option<E8sPerToken>,
         time: Timestamp,
     ) -> Result<(), String> {
-        if amount < *self.icp_pool.get(&buyer).unwrap_or(&0) {
+        let max_fee = amount * 15 / 10000;
+        if amount + max_fee < *self.icp_pool.get(&buyer).unwrap_or(&0) {
             return Err("not enough ICP funds".into());
         }
 
         let sell_orders = &mut self
             .orders
             .get_mut(&token)
-            .ok_or("no orders found")?
+            .ok_or("no sell orders found")?
             .sellers;
 
         let archive = self.order_archive.entry(token).or_default();
 
         while let Some(mut order) = sell_orders.pop_first() {
-            // check the limit
-            if let Some(limit) = limit {
-                if limit > order.price {
-                    sell_orders.insert(order);
-                    break;
-                }
+            let volume = order.amount * order.price;
+            if limit > Some(order.price) || volume > amount {
+                sell_orders.insert(order);
+                break;
             }
 
-            let e8s = order.amount * order.price;
-            amount = match amount.checked_sub(e8s) {
-                // if we detect an order larger than buyer's funds, stop
-                None => {
-                    sell_orders.insert(order);
-                    break;
-                }
-                Some(new_amount) => new_amount,
-            };
+            amount = amount.checked_sub(volume).expect("amount is too small");
 
-            // adjust token pool amounts
-            let token_pool = self.pools.get_mut(&token).expect("no token pool found");
-            let sellers_tokens = token_pool.get_mut(&order.owner).expect("no tokens in pool");
-            *sellers_tokens = sellers_tokens
-                .checked_sub(order.amount)
-                .expect("not enough tokens");
-            let buyers_tokens = token_pool.entry(buyer).or_insert(0);
-            *buyers_tokens += order.amount;
-
-            let fee = e8s * 15 / 10000;
-
-            // adjust icp pool amounts
-            let buyers_icp_tokens = self.icp_pool.get_mut(&buyer).expect("no ICP tokens");
-            *buyers_icp_tokens = buyers_icp_tokens
-                .checked_sub(e8s + fee)
-                .expect("not enough ICP tokens");
-            let sellers_icp_tokens = self.icp_pool.entry(buyer).or_default();
-            *sellers_icp_tokens += e8s.checked_sub(fee).expect("amount smaller than fees");
-            let icp_fees = self
-                .icp_pool
-                .entry(self.revenue_account.expect("no revenue account set"))
-                .or_default();
-            *icp_fees += 2 * fee;
+            trade(
+                &mut self.pools,
+                &mut self.icp_pool,
+                order.owner,
+                buyer,
+                token,
+                order.amount,
+                volume,
+                self.revenue_account.unwrap(),
+            );
 
             order.executor = Some(buyer);
             order.executed = time;
@@ -158,4 +190,36 @@ impl State {
 
         Ok(())
     }
+}
+
+fn trade(
+    pools: &mut BTreeMap<TokenId, BTreeMap<Principal, Tokens>>,
+    icp_pool: &mut BTreeMap<Principal, E8s>,
+    seller: Principal,
+    buyer: Principal,
+    token: TokenId,
+    amount: Tokens,
+    volume: E8s,
+    revenue_account: Principal,
+) {
+    // adjust token pool amounts
+    let token_pool = pools.get_mut(&token).expect("no token pool found");
+    let sellers_tokens = token_pool.get_mut(&seller).expect("no tokens in pool");
+    *sellers_tokens = sellers_tokens
+        .checked_sub(amount)
+        .expect("not enough tokens");
+    let buyers_tokens = token_pool.entry(buyer).or_insert(0);
+    *buyers_tokens += amount;
+
+    let fee = volume * TX_FEE / 10000;
+
+    // adjust icp pool amounts
+    let buyers_icp_tokens = icp_pool.get_mut(&buyer).expect("no ICP tokens");
+    *buyers_icp_tokens = buyers_icp_tokens
+        .checked_sub(volume + fee)
+        .expect("not enough ICP tokens");
+    let sellers_icp_tokens = icp_pool.entry(buyer).or_default();
+    *sellers_icp_tokens += volume.checked_sub(fee).expect("amount smaller than fees");
+    let icp_fees = icp_pool.entry(revenue_account).or_default();
+    *icp_fees += 2 * fee;
 }
