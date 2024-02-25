b6554e4a37936a64fa564e9d7425e6abdf3107e6
X <x@taggr.link>
2024-02-25 17:24:57 +0100

    chore: separate routines

--- src/backend/lib.rs
+++ src/backend/lib.rs
@@ -96,6 +96,8 @@ fn kickstart() {
     set_timer_interval(Duration::from_secs(15 * 60), fetch_rate);
     set_timer_interval(Duration::from_secs(60 * 60), || {
         mutate(|state| state.clean_up(ic_cdk::api::time()));
+    });
+    set_timer_interval(Duration::from_secs(60 * 60), || {
         mutate(heap_to_stable);
     });
 }
